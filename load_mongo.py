bebe0790f38db4d3287102209e7fc9d465297b11
mjnb4042 <cedric.guadalupe@orange.com>
2020-03-09 15:24:35 +0100

    CGU - Ajout du fichier recovered

--- load_mongo.py
+++ load_mongo.py
@@ -12,13 +12,15 @@ france_covid19 = db.france_covid19
 
 df_confirmed = pd.read_csv("france_coronavirus_time_series-confirmed.csv", sep=";")
 df_deaths = pd.read_csv("france_coronavirus_time_series-deaths.csv", sep=";")
+df_recovered = pd.read_csv("france_coronavirus_time_series-recovered.csv", sep=";")
 df_confirmed["Type"] = "Confirmed"
 df_deaths["Type"] = "Death"
-df_joined = pd.concat([df_confirmed, df_deaths]).set_index(["Date","Type"]).stack().reset_index()
+df_recovered["Type"] = "Recovered"
+df_joined = pd.concat([df_confirmed, df_deaths, df_recovered]).set_index(["Date", "Type"]).stack().reset_index()
 df_joined["Value"] = df_joined[0]
 df_joined["Region"] = df_joined["level_2"]
 df_joined["Region_Code"] = df_joined["Region"].str.upper().apply(unidecode.unidecode)
-for row in df_joined.pivot_table(index=["Date","Region","Region_Code"],columns="Type",values="Value").replace({np.NAN: None}).reset_index().to_dict("records"):
+for row in df_joined.pivot_table(index=["Date", "Region", "Region_Code"], columns="Type", values="Value").replace({np.NAN: None}).reset_index().to_dict("records"):
     date_value = datetime.strptime(row["Date"], "%d/%m/%Y")
     france_covid19.update({
         "Date": date_value,
